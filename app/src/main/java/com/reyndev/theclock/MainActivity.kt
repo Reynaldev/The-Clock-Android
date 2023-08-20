7ee66d04c27092b0769fb4baec2056df3295a3a4
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-08-20 09:37:08 +0700

    Update
    New
    - Added function to add a zero when the time has length less than 2

--- app/src/main/java/com/reyndev/theclock/MainActivity.kt
+++ app/src/main/java/com/reyndev/theclock/MainActivity.kt
@@ -2,24 +2,22 @@ package com.reyndev.theclock
 
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.reyndev.theclock.databinding.ActivityMainBinding
 import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.asCoroutineDispatcher
+import java.lang.StringBuilder
 import java.util.Calendar
 import java.util.Date
 import java.util.GregorianCalendar
-import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var calendar: GregorianCalendar
 
-    private var hour: Int = 0
-    private var minute: Int = 0
-    private var second: Int = 0
+    private var hour: String = "00"
+    private var minute: String = "00"
+    private var second: String = "00"
 
     private var isResumed: Boolean = true
 
@@ -34,14 +32,23 @@ class MainActivity : AppCompatActivity() {
             override fun run() {
                 calendar.time = Date()
 
-                hour = calendar.get(Calendar.HOUR_OF_DAY)
-                minute = calendar.get(Calendar.MINUTE)
-                second = calendar.get(Calendar.SECOND)
+                hour = timeToString(calendar.get(Calendar.HOUR_OF_DAY))
+                minute = timeToString(calendar.get(Calendar.MINUTE))
+                second = timeToString(calendar.get(Calendar.SECOND))
 
                 binding.clockText.text = "${hour}:${minute}:${second}"
 
                 Handler().postDelayed(this, 500)
             }
         })
+    }
+
+    fun timeToString(time: Int): String {
+        var timeStr = StringBuilder(time.toString())
+
+        if (timeStr.length < 2)
+            return timeStr.insert(0, '0').toString()
+        else
+            return timeStr.toString()
     }
 }
